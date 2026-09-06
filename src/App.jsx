c5331a3fe8e05397e1f9428bbc4e8c0b6640852f
AswinCodie <aswinasus233@gmail.com>
2026-09-06 19:46:38 +0530

    User route protected

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import SignUp from './Pages/auth/SignUp'
-import UserProfile from './Pages/UserProfile'
-import Login from './Pages/auth/Login'
-import AdminPanel from './Pages/AdminPanel'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import SignUp from "./Pages/auth/SignUp";
+import UserProfile from "./Pages/UserProfile";
+import Login from "./Pages/auth/Login";
+import AdminPanel from "./Pages/AdminPanel";
+import ProtectedRoute from "./Pages/ProtectedRoute";
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/users" element={<UserProfile />} />
-        <Route path='/admin' element={<AdminPanel/>}/> 
-      </Routes>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
 
-export default App
+      {/* Only users can access /users */}
+      <Route
+        path="/users"
+        element={
+          <ProtectedRoute role="user">
+            <UserProfile />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Only admin can access /admin */}
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute role="admin">
+            <AdminPanel />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/Pages/ProtectedRoutes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children, role }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  // Not logged in
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // Logged in, but wrong role
+  if (user.role !== role) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
