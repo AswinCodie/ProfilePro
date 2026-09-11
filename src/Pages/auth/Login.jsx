aeb53960f1dc4fc89c3306716a94be758dd3e7e0
AswinCodie <aswinasus233@gmail.com>
2026-09-11 11:08:06 +0530

    login

--- src/Pages/auth/Login.jsx
+++ src/Pages/auth/Login.jsx
@@ -51,7 +51,7 @@ const Login = () => {
   return (
     <div className="main-container">
       <div className="container">
-        <h1>Login Here</h1>
+        <h1>Login</h1>
 
         <p className="auth-subtitle">
           Welcome back! Please login to your account.
